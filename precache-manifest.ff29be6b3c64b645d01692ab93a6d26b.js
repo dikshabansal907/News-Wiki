695d0d01b4cf7b540d889e5fac1529c0c9ca68d8
Diksha <dikshabansal907@gmail.com>
2022-03-11 02:01:32 +0530

    Updates

---
+++ precache-manifest.ff29be6b3c64b645d01692ab93a6d26b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bedf8dca7a288ecba7c3746272c38e6",
+    "url": "/News-Wiki/index.html"
+  },
+  {
+    "revision": "a85c2eee318cb8a15d4d",
+    "url": "/News-Wiki/static/css/main.913e0651.chunk.css"
+  },
+  {
+    "revision": "ec98cabba5b70c589269",
+    "url": "/News-Wiki/static/js/2.f215038c.chunk.js"
+  },
+  {
+    "revision": "5d06bb858cc186fdb179",
+    "url": "/News-Wiki/static/js/3.b019360d.chunk.js"
+  },
+  {
+    "revision": "a85c2eee318cb8a15d4d",
+    "url": "/News-Wiki/static/js/main.7b1808c6.chunk.js"
+  },
+  {
+    "revision": "418f55640fc1149dae04",
+    "url": "/News-Wiki/static/js/runtime~main.14266260.js"
+  },
+  {
+    "revision": "3dc6a4f579d6a2effdd3d27568acbc28",
+    "url": "/News-Wiki/static/media/loading.3dc6a4f5.gif"
+  },
+  {
+    "revision": "482abc5ff3f39e675e2c5d295634e4db",
+    "url": "/News-Wiki/static/media/newspaper.482abc5f.jpeg"
+  }
+]);
